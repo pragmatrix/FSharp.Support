58779ed86ab7545ed09410250ba12b3be3a808f0
Armin Sander <armin@replicator.org>
2016-07-15 17:47:03 +0200

    extracted the assembly builder to prepare for multi assembly compilation

---
+++ LivePipes.FSharp/ProjectOptions.fs
@@ -0,0 +1,29 @@
+﻿module LivePipes.FSharp.ProjectOptions
+
+open System.IO
+open Microsoft.FSharp.Compiler.SourceCodeServices
+
+/// Helper for FSharpProjectOptions
+type private Options = FSharpProjectOptions
+
+[<Literal>]
+let OutputFilePrefix = "--out:"
+[<Literal>]
+let AssemblyReferencePrefix = "-r:"
+         
+let outputDirectory (options: Options) = 
+    options.OtherOptions
+    |> Seq.find (fun str -> str.StartsWith(OutputFilePrefix))
+    |> fun str -> str.Substring(OutputFilePrefix.Length)
+    |> Path.GetDirectoryName
+
+let assemblyReferences (options: Options) =
+    options.OtherOptions
+    |> Seq.filter (fun str -> str.StartsWith(AssemblyReferencePrefix))
+    |> Seq.map (fun str -> str.Substring(AssemblyReferencePrefix.Length))
+    |> Seq.toList
+
+let projectFileNames options = 
+    options.OtherOptions
+    |> Seq.filter (fun str -> not <| str.StartsWith("-") && str.ToLowerInvariant().EndsWith(".fs"))
+    |> Seq.toList
