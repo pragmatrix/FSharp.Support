8ed7b594265d7db8ef39b96999882ee229455d63
Armin Sander <armin@replicator.org>
2016-08-07 01:56:28 +0200

    finalize the incremental compilation strategy

---
+++ LivePipes.FSharp.Tests/ProjectOptionsTests.fs
@@ -0,0 +1,22 @@
+﻿module ProjectOptionsTests
+
+open FsUnit
+open Xunit
+open LivePipes.FSharp
+
+[<Fact>]
+let projectOptionsCanBeConvertedToFSharpOptionsAndBackWithoutLosingInformation() =
+    let options = {
+        ProjectFile = "test.fsproj"
+        SourceFiles = ["Hello.fs"; "World.fs"]
+        AssemblyReferences = ["a.dll"; "b.dll"]
+        OutputFile = "c.dll"
+        Options = ["--opt1"; "--opt2"]
+    }
+
+    let fs = options |> ProjectOptions.toFSharpOptions
+    let o2 = fs |> ProjectOptions.fromFSharpOptions
+    o2 |> should equal options
+
+        
+
--- LivePipes.FSharp/ProjectOptions.fs
+++ LivePipes.FSharp/ProjectOptions.fs
@@ -1,33 +1,74 @@
-﻿module LivePipes.FSharp.ProjectOptions
+﻿namespace LivePipes.FSharp
 
+open System
 open System.IO
 open Microsoft.FSharp.Compiler.SourceCodeServices
 
-/// Helper for FSharpProjectOptions
-type private Options = FSharpProjectOptions
+type ProjectOptions = {
+    ProjectFile: string
+    OutputFile: string
+    SourceFiles: string list
+    AssemblyReferences: string list
+    Options: string list
+} with
+    member this.OutputDirectory =
+        this.OutputFile |> Path.GetDirectoryName
+        
 
-[<Literal>]
-let OutputFilePrefix = "--out:"
-[<Literal>]
-let AssemblyReferencePrefix = "-r:"
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module ProjectOptions =
 
-let outputFile (options: Options) = 
-    options.OtherOptions
-    |> Seq.find (fun str -> str.StartsWith(OutputFilePrefix))
-    |> fun str -> str.Substring(OutputFilePrefix.Length)
+    let empty = {
+        ProjectFile = ""
+        OutputFile = ""
+        SourceFiles = []
+        AssemblyReferences = []
+        Options = []
+    }
 
-let outputDirectory (options: Options) = 
-    options
-    |> outputFile
-    |> Path.GetDirectoryName
+    let (|Prefixed|_|) (prefix: string) (input: string) =
+        if input.Length >= prefix.Length && input.[0..prefix.Length-1] = prefix then
+            Some (input.[prefix.Length..])
+        else
+            None
 
-let assemblyReferences (options: Options) =
-    options.OtherOptions
-    |> Seq.filter (fun str -> str.StartsWith(AssemblyReferencePrefix))
-    |> Seq.map (fun str -> str.Substring(AssemblyReferencePrefix.Length))
-    |> Seq.toList
+    [<Literal>]
+    let OutputFilePrefix = "--out:"
+    let AssemblyReferencePrefix = "-r:"
 
-let projectFileNames options = 
-    options.OtherOptions
-    |> Seq.filter (fun str -> not <| str.StartsWith("-") && str.ToLowerInvariant().EndsWith(".fs"))
-    |> Seq.toList
+    let fromFSharpOptions (options: FSharpProjectOptions) : ProjectOptions = 
+        let processOption option options = 
+            match option with
+            | Prefixed OutputFilePrefix outputFile ->
+                { options with OutputFile = outputFile }
+            | Prefixed AssemblyReferencePrefix reference ->
+                { options with AssemblyReferences = reference :: options.AssemblyReferences}
+            | sourceFile when not (option.StartsWith("-")) && (option.ToLowerInvariant().EndsWith(".fs")) ->
+                { options with SourceFiles = sourceFile :: options.SourceFiles }
+            | option ->
+                { options with Options = option :: options.Options }
+    
+        Array.foldBack processOption options.OtherOptions empty
+        |> fun o -> { o with ProjectFile = options.ProjectFileName }
+
+    let toFSharpOptions (options: ProjectOptions) : FSharpProjectOptions = 
+        
+        let otherOptions =
+            [
+                yield! options.SourceFiles
+                yield! options.AssemblyReferences |> List.map (fun str -> AssemblyReferencePrefix + str)
+                yield OutputFilePrefix + options.OutputFile
+                yield! options.Options
+            ]
+
+        {
+            ProjectFileName = options.ProjectFile
+            ProjectFileNames = [||]
+            OtherOptions = otherOptions |> List.toArray
+            ReferencedProjects = [||]
+            IsIncompleteTypeCheckEnvironment = false
+            UseScriptResolutionRules = false
+            LoadTime = DateTime.Now
+            UnresolvedReferences = None
+        }
+        
