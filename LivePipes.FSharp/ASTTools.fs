903fe2009ed72bfd15c5ede50a13d53465fc51a9
Armin Sander <armin@replicator.org>
2016-08-13 23:49:54 +0200

    add ASTTools, which should contains specific AST transformations required for live compilation

---
+++ LivePipes.FSharp/ASTTools.fs
@@ -0,0 +1,34 @@
+﻿/// Tools for the untyped AST
+module LivePipes.FSharp.ASTTools
+
+open Microsoft.FSharp.Compiler.Ast
+
+module RemoveEntryPointAttribute =
+
+    let rewriteBinding (Binding(access, bindingKind, mustInline, isMutable, attributes, xmlDoc, valData, headPat, returnInfo, expr, lhsRange, spBind)) =
+        Binding(access, bindingKind, mustInline, isMutable, attributes, xmlDoc, valData, headPat, returnInfo, expr, lhsRange, spBind)
+
+    let rec rewriteDeclaration (declaration: SynModuleDecl) : SynModuleDecl =
+        match declaration with
+        | SynModuleDecl.NestedModule (sci, isRec, declarations, b, r) -> 
+            SynModuleDecl.NestedModule (sci, isRec, declarations |> List.map rewriteDeclaration, b, r)
+        | SynModuleDecl.Let (b, bindings, r) ->
+            SynModuleDecl.Let (b, bindings |> List.map rewriteBinding, r)
+        | SynModuleDecl.NamespaceFragment moduleOrNamespace ->
+            SynModuleDecl.NamespaceFragment (rewriteModuleOrNamespace moduleOrNamespace)
+        | _ -> declaration
+
+    and rewriteModuleOrNamespace (SynModuleOrNamespace(lid, isRec, isModule, declarations, xmlDoc, attributes, synAccess, range)) =
+        let declarations = declarations |> List.map rewriteDeclaration
+        (SynModuleOrNamespace(lid, isRec, isModule, declarations, xmlDoc, attributes, synAccess, range))
+    
+    let rewriteParsedInput (parsedInput: ParsedInput) = 
+        match parsedInput with
+        | ParsedInput.ImplFile(implFile) ->
+            let (ParsedImplFileInput(filename, isScript, name, scopedPragmas, parsedHashDirectives, modules, twoFlags)) = implFile
+            let modules = modules |> List.map rewriteModuleOrNamespace
+            ParsedImplFileInput(filename, isScript, name, scopedPragmas, parsedHashDirectives, modules, twoFlags)
+            |> ParsedInput.ImplFile
+
+        | _ -> parsedInput
+    
