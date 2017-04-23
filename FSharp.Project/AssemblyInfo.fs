5a695266418f0a43cb47a3d47efcc25fd59e0fe7
Armin Sander <armin@replicator.org>
2017-04-23 11:26:27 +0200

    build a project cracker tool library, so that we can debug what the project cracker does and don't have to start a separate process

---
+++ FSharp.Project/AssemblyInfo.fs
@@ -0,0 +1,23 @@
+﻿namespace FSharp.Project.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyTitle("FSharp.Project")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("FSharp.Project")>]
+[<assembly: AssemblyCopyright("Copyright © 2017")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+[<assembly: ComVisible(false)>]
+
+[<assembly: Guid("71c870db-169e-45a1-a9e5-e4b73a71da70")>]
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
